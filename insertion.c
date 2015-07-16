2564e477c621a8f2aa4bd4fd7b29605eeadf2a32
iZwag <jon.zk@hotmail.com>
2015-07-17 00:37:43 +0200

    Added first, unfinished version of insertion.c

---
+++ insertion.c
@@ -0,0 +1,27 @@
+#include <stdint.h>
+#include <stdio.h>
+
+uint32_t i, j, array_size;
+uint8_t array[];
+
+uint8_t key;
+
+int main(int argc, char* argv[])
+{
+   /*  */
+   array = [12,16,4,11,3,9,8,45,28,2,13,30,22];
+   array_size = 1;
+   j=0;
+   while(array[j] =! )
+   
+   
+   for(j=2;j<=)
+   {
+   
+
+
+   }
+
+}
+
+
