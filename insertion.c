ff26761ea783383a9ea198f286c7889fb1036273
iZwag <jon.zk@hotmail.com>
2015-07-17 15:13:20 +0200

    Finished first version. Naive array-filling.

--- insertion.c
+++ insertion.c
@@ -3,25 +3,44 @@
 
 uint32_t i, j, array_size;
 uint8_t array[];
-
 uint8_t key;
 
 int main(int argc, char* argv[])
 {
-   /*  */
+   /* Hardcoded array parameters  */
    array = [12,16,4,11,3,9,8,45,28,2,13,30,22];
-   array_size = 1;
-   j=0;
-   while(array[j] =! )
+   array_size = 13;
+
+   printf("Unsorted array: ");
+   for(j=0;j<array_size;j++)
+   {      
+      printf("%u, ", array[j]);
+   }
+   printf("\n");
    
-   
-   for(j=2;j<=)
+   /* Insertion sort algorithm */
+   for(j=2;j<=array_size;j++)
    {
-   
+      key = array[j];
+      i = j - 1;
 
-
+      while( (i>0) && (array[i])>key)
+      {
+         array[i + 1] = array[i];
+         i -= 1;
+      }
+      
+      array[i+1] = key;
    }
 
+   printf("Sorted array: ");
+   for(j=0;j<array_size;j++)
+   {      
+      printf("%u, ", array[j]);
+   }
+   printf("\n");
+
+   return 0;
 }
 
 
