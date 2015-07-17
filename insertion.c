72cc129b50795f538c94555fb8f73e50d0a61c35
iZwag <jon.zk@hotmail.com>
2015-07-17 17:00:22 +0200

    Insertion sort works, but has problems with 1st element

--- insertion.c
+++ insertion.c
@@ -1,42 +1,48 @@
 #include <stdint.h>
+#include <stdlib.h>
 #include <stdio.h>
-
-uint32_t i, j, array_size;
-uint8_t array[];
-uint8_t key;
 
 int main(int argc, char* argv[])
 {
-   /* Hardcoded array parameters  */
-   array = [12,16,4,11,3,9,8,45,28,2,13,30,22];
-   array_size = 13;
+   unsigned int i, j, key; 
 
+   /* Hardcoded array parameters  */
+   unsigned int array_size = 13;
+   unsigned int arr[] = {12,16,4,11,3,9,8,45,28,2,13,30,22};
+   
+   /* Display array */
    printf("Unsorted array: ");
    for(j=0;j<array_size;j++)
    {      
-      printf("%u, ", array[j]);
+      printf("%u, ", arr[j]);
    }
    printf("\n");
    
-   /* Insertion sort algorithm */
-   for(j=2;j<=array_size;j++)
-   {
-      key = array[j];
-      i = j - 1;
 
-      while( (i>0) && (array[i])>key)
+   /* INSERTION SORT */
+   for(j=1;j<=array_size;j++)
+   {
+      // Place current arrayelement in key
+      key = arr[j];
+      // i is the index before current
+      i = j - 1;
+      
+      while( (i>0) && (arr[i])>key)
       {
-         array[i + 1] = array[i];
+         arr[i + 1] = arr[i];
          i -= 1;
       }
-      
-      array[i+1] = key;
+         
+      arr[i+1] = key;          
+    
    }
 
+
+   /* Display sorted array */
    printf("Sorted array: ");
    for(j=0;j<array_size;j++)
    {      
-      printf("%u, ", array[j]);
+      printf("%u, ", arr[j]);
    }
    printf("\n");
 
