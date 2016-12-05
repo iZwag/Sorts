1aa5835a5a9ff54e7fd460760a77f38586df48f5
iZwag <jon.zk@hotmail.com>
2016-12-05 01:38:07 +0100

    Merge sort working and tested. Needs code optimization, but is decent enough for upload.

--- merge/merge.c
+++ merge/merge.c
@@ -1,13 +1,58 @@
 #include "merge.h"
 
+void split(uint16_t* list, uint16_t f_element, uint16_t l_element);
+void merge(uint16_t* list, uint16_t f_element, uint16_t m_element, uint16_t l_element);
+uint16_t middle_element(uint16_t first, uint16_t last);
+
+/* API FUNCTIONS */
+
 void merge_sort(uint16_t* list, uint16_t size)
 {
-   int i;
+   split(list, 0, size-1);
+}
 
-   for(i=0; i<size; i++)
-   {
-      list[i]= 2*list[i];
-      printf("%d ", list[i]);  
+/* LOCAL FUNCTIONS */
+
+void split(uint16_t* list, uint16_t f_element, uint16_t l_element)
+{
+   if(f_element < l_element){
+      split(list, f_element, middle_element(f_element, l_element));
+      split(list, middle_element(f_element, l_element) + 1, l_element);
+      merge(list, f_element, middle_element(f_element, l_element), l_element);
    }
-   printf("\n");
+}
+
+void merge(uint16_t* list, uint16_t f_element, uint16_t m_element, uint16_t l_element)
+{
+   uint16_t left_list  [(m_element - f_element + 1) + 1];
+   uint16_t right_list [(l_element - m_element) + 1];
+   uint16_t i, j, k;
+
+   for(i = 1; i < ((m_element - f_element + 1) + 1); i++){
+      left_list[i] = list[f_element + i - 1];
+   }
+   for(j = 1; j < ((l_element - m_element) + 1); j++){
+      right_list[j] = list[m_element + j];
+   }
+
+   left_list[(m_element - f_element + 1) + 1] = 0xFFFF;
+   right_list[(l_element - m_element) + 1] = 0xFFFF;
+
+   i = j = 1;
+
+   for(k = f_element; k<= l_element; k++){
+      if(left_list[i] <= right_list[j]){
+         list[k] = left_list[i];
+         i++;
+      }
+      else{
+         list[k] = right_list[j];
+         j++;
+      }
+   }
+}
+
+uint16_t middle_element(uint16_t first, uint16_t last)
+{
+   return ( (first + last)/2 ); 
 }
--- merge/merge.h
+++ merge/merge.h
@@ -1,3 +1,16 @@
+/* ***************************************************************************
+ * MERGE SORT
+ * - Stable (does not change position of equal numbered elements)
+ * - Not in-place (requires extra memory other than the original list)
+ * - Recursive, divide-and-conquer
+ *
+ * Programmed by Jon Zwaig Kolstad, december 2016
+ *
+ * Parameters:
+ *    list - Array-list of unsigned integers. Can take any values 0 - 0xFFFE
+ *    size - Number of elements in the list.                                                                            
+ *****************************************************************************/
+
 #ifndef MERGE_H
 #define MERGE_H
 
