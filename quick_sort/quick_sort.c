08ab0961610dca0329d82e193b73eb6efa7f31b1
iZwag <jon.zk@hotmail.com>
2017-02-18 19:42:48 +0100

    Created the early foundation.

---
+++ quick_sort/quick_sort.c
@@ -0,0 +1,11 @@
+#include "quick_sort.h"
+
+/* API FUNCTIONS */
+
+void quick_sort(uint16_t* list, uint16_t size)
+{
+   
+}
+
+/* LOCAL FUNCTIONS */
+
---
+++ quick_sort/quick_sort.h
@@ -0,0 +1,19 @@
+/* ***************************************************************************
+ * QUICK SORT
+ *
+ * Programmed by Jon Zwaig Kolstad, February 2017
+ *
+ * Parameters:
+ *    list - Array-list of unsigned integers.
+ *    size - Number of elements in the list.                                                                            
+ *****************************************************************************/
+
+#ifndef QUICK_SORT_H
+#define QUICK_SORT_H
+
+#include <stdio.h>
+#include <stdint.h>
+
+void quick_sort(uint16_t* list, uint16_t size);
+
+#endif
