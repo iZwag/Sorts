cb8cb8254c692d35671e161f26a6223b14f6b76a
iZwag <jon.zk@hotmail.com>
2017-03-07 12:18:51 +0100

    Working sor, but with a lot of prints for the recent debugging.

--- quick_sort/quick_sort.c
+++ quick_sort/quick_sort.c
@@ -3,11 +3,14 @@
 #include <time.h>
 #include <stdlib.h>
 
+#include <stdio.h>
+#include <inttypes.h>
+
 void                split (uint16_t* list, uint16_t f_index, uint16_t l_index);
 uint16_t        partition (uint16_t* list, uint16_t f_index, uint16_t l_index);
 uint16_t random_partition (uint16_t* list, uint16_t f_index, uint16_t l_index);
 
-void swap (uint16_t *a, uint16_t *b);
+void swap (uint16_t* a, uint16_t* b);
 
 /*****************************************************************************/
 /*                              API FUNCTIONS                                */
@@ -19,6 +22,7 @@ void quick_sort(uint16_t* list, uint16_t size)
    srand(time(NULL));
 
    // Initial split, then recurisively splits and sorts
+   printf("Starting sort with size %" PRIu16 "!\n\r", size);
    split(list, 0, size-1);
 }
 
@@ -28,12 +32,17 @@ void quick_sort(uint16_t* list, uint16_t size)
 
 void split(uint16_t* list, uint16_t f_index, uint16_t l_index)
 {
-   uint16_t divider;
+   printf("In SPLIT, received indices %" PRIu16 " and %" PRIu16 ".\n", f_index, l_index);
 
    if(f_index < l_index){
 
-      divider = random_partition(list, f_index, l_index);
-      split(list, f_index, divider - 1);
+      int divider = random_partition(list, f_index, l_index);
+      //printf("In SPLIT, new divider returned: %" PRIu16 " \n", divider);
+      printf("Splitting into two subarrays... \n");
+
+      if(divider != 0){
+         split(list, f_index, divider - 1);
+      }
       split(list, divider + 1, l_index);
 
    }
@@ -41,38 +50,48 @@ void split(uint16_t* list, uint16_t f_index, uint16_t l_index)
 
 uint16_t partition(uint16_t* list, uint16_t f_index, uint16_t l_index)
 {
-   uint16_t pivot, temp, i, j;
+   uint16_t pivot, j, z;
+
+   int i;
 
    pivot = list[l_index];
    i = f_index - 1;
 
-   for(j = f_index; j <= (l_index - 1); j++){
+   printf("Now partitioning between: %" PRIu16 " and %" PRIu16 " .\n\r", f_index, l_index);
+
+   for(j = f_index; j < l_index; j++){
 
       if(list[j] <= pivot){
 
          i++;
-         swap(&list[i], &list[j]);
+         swap(&list[(uint16_t) i], &list[j]);
 
       }
    }
 
-   swap(&list[l_index], &list[i + 1]);
+   z = (uint16_t) i + 1;
 
-   return (i + 1);
+   printf("About to swap element %" PRIu16 " and %" PRIu16 ".\n", z, l_index);
+   printf("They correspond to the values %" PRIu16 " and %" PRIu16 ".\n", list[i+1], list[l_index]);
+   swap(&list[l_index], &list[z]);
+
+   return z;
 }
 
 uint16_t random_partition(uint16_t* list, uint16_t f_index, uint16_t l_index)
 {
-   uint16_t r_index, temp;
+   uint16_t r_index;
+
+   printf("Now finding a random partition between %" PRIu16 " and %" PRIu16 "...\n", f_index, l_index);
 
    r_index = (rand() % ((l_index - f_index) + 1)) + f_index;
+   printf("The random number: %" PRIu16 " \n", r_index);
    swap(&list[l_index], &list[r_index]);
 
    return partition(list, f_index, l_index);
 }
 
-// Good
-void swap(uint16_t *a, uint16_t *b)
+void swap(uint16_t* a, uint16_t* b)
 {
    uint16_t temp = *a;
    *a = *b;
