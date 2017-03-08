36b2462e47e6be2765e3fa5e2683c5111338ac04
iZwag <jon.zk@hotmail.com>
2017-03-08 01:02:52 +0100

    Added styling from quick sort, cleaned up variable names. TODO: Remove max-value-sentinel dependencies.

--- merge_sort/merge_sort.c
+++ merge_sort/merge_sort.c
@@ -1,46 +1,50 @@
 #include "merge_sort.h"
 
-void split(uint16_t* list, uint16_t f_element, uint16_t l_element);
-void merge(uint16_t* list, uint16_t f_element, uint16_t m_element, uint16_t l_element);
+void split(uint16_t* list, uint16_t f_index, uint16_t l_index);
+void merge(uint16_t* list, uint16_t f_index, uint16_t m_index, uint16_t l_index);
 uint16_t middle_element(uint16_t first, uint16_t last);
 
-/* API FUNCTIONS */
+/*****************************************************************************/
+/*                              API FUNCTIONS                                */
+/*****************************************************************************/
 
 void merge_sort(uint16_t* list, uint16_t size)
 {
    split(list, 0, size-1);
 }
 
-/* LOCAL FUNCTIONS */
+/*****************************************************************************/
+/*                             LOCAL FUNCTIONS                               */
+/*****************************************************************************/
 
-void split(uint16_t* list, uint16_t f_element, uint16_t l_element)
+void split(uint16_t* list, uint16_t f_index, uint16_t l_index)
 {
-   if(f_element < l_element){
-      split(list, f_element, middle_element(f_element, l_element));
-      split(list, middle_element(f_element, l_element) + 1, l_element);
-      merge(list, f_element, middle_element(f_element, l_element), l_element);
+   if(f_index < l_index){
+      split(list, f_index, middle_element(f_index, l_index));
+      split(list, middle_element(f_index, l_index) + 1, l_index);
+      merge(list, f_index, middle_element(f_index, l_index), l_index);
    }
 }
 
-void merge(uint16_t* list, uint16_t f_element, uint16_t m_element, uint16_t l_element)
+void merge(uint16_t* list, uint16_t f_index, uint16_t m_index, uint16_t l_index)
 {
-   uint16_t left_list  [(m_element - f_element + 1) + 1];
-   uint16_t right_list [(l_element - m_element) + 1];
+   uint16_t left_list  [(m_index - f_index + 1) + 1];
+   uint16_t right_list [(l_index - m_index) + 1];
    uint16_t i, j, k;
 
-   for(i = 1; i < ((m_element - f_element + 1) + 1); i++){
-      left_list[i] = list[f_element + i - 1];
+   for(i = 1; i < ((m_index - f_index + 1) + 1); i++){
+      left_list[i] = list[f_index + i - 1];
    }
-   for(j = 1; j < ((l_element - m_element) + 1); j++){
-      right_list[j] = list[m_element + j];
+   for(j = 1; j < ((l_index - m_index) + 1); j++){
+      right_list[j] = list[m_index + j];
    }
 
-   left_list[(m_element - f_element + 1) + 1] = 0xFFFF;
-   right_list[(l_element - m_element) + 1] = 0xFFFF;
+   left_list[(m_index - f_index + 1) + 1] = 0xFFFF;
+   right_list[(l_index - m_index) + 1] = 0xFFFF;
 
    i = j = 1;
 
-   for(k = f_element; k<= l_element; k++){
+   for(k = f_index; k<= l_index; k++){
       if(left_list[i] <= right_list[j]){
          list[k] = left_list[i];
          i++;
